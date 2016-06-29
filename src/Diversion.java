e3e1b2398f8c429654d5750bed1496bd3af8e200
shinesoleil <hao_pan@outlook.com>
2016-06-29 11:44:54 +0800

    diversion done

--- src/Diversion.java
+++ src/Diversion.java
@@ -1,6 +1,19 @@
 
 public class Diversion {
     public static int count(int digitNumber) {
-        return 0;
+        if (digitNumber == 1) {
+            return 0;
+        }
+
+        int countEndWithZero = 1;
+        int countEndWithOne = 1;
+
+        for (int i = 0; i < digitNumber - 1; i++) {
+            int previousCountEndWithZero = countEndWithZero;
+            countEndWithZero = countEndWithZero + countEndWithOne;
+            countEndWithOne = previousCountEndWithZero;
+        }
+
+        return countEndWithOne + countEndWithZero;
     }
 }
--- test/DiversionTest.java
+++ test/DiversionTest.java
@@ -8,13 +8,18 @@ public class DiversionTest {
         assertEquals(0, Diversion.count(1));
     }
 
-//    @Test
-//    public void testTwoDigits() {
-//        assertEquals(3, Diversion.count(2));
-//    }
-//
-//    @Test
-//    public void testThreeDigits() {
-//        assertEquals(5, Diversion.count(3));
-//    }
+    @Test
+    public void testTwoDigits() {
+        assertEquals(3, Diversion.count(2));
+    }
+
+    @Test
+    public void testThreeDigits() {
+        assertEquals(5, Diversion.count(3));
+    }
+
+    @Test
+    public void testFourDigits() {
+        assertEquals(8, Diversion.count(4));
+    }
 }
